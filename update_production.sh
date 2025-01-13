7be2608e75ff60b39585ac80d810b93d2efdbeb5
Niklas Mohrin <dev@niklasmohrin.de>
2025-01-13 19:39:25 +0100

    Update usages of maintenance mode scripts

--- update_production.sh
+++ update_production.sh
@@ -29,7 +29,7 @@ set -x # print executed commands. enable this here to not print the if above.
 # Note that apache should not be running during most of the upgrade,
 # since then e.g. the backup might be incomplete or the code does not
 # match the database layout, or https://github.com/e-valuation/EvaP/issues/1237.
-[[ -z "$EVAP_SKIP_APACHE_STEPS" ]] && sudo ./deployment/enable_maintenance_mode.sh
+[[ -z "$EVAP_SKIP_APACHE_STEPS" ]] && sudo ./maintenance_mode.sh enable
 
 python -m evap dumpdata --natural-foreign --natural-primary --all -e contenttypes -e auth.Permission --indent 2 --output "$FILENAME"
 
@@ -37,11 +37,13 @@ python -m evap dumpdata --natural-foreign --natural-primary --all -e contenttype
 [[ ! -z "$EVAP_SKIP_UPDATE" ]] || pip install evap[psycopg-binary]
 
 python -m evap collectstatic --noinput
+
 python -m evap migrate
+
 python -m evap clear_cache --all -v=1
 python -m evap refresh_results_cache
 
-[[ -z "$EVAP_SKIP_APACHE_STEPS" ]] && sudo ./deployment/disable_maintenance_mode.sh
+[[ -z "$EVAP_SKIP_APACHE_STEPS" ]] && sudo ./maintenance_mode.sh disable
 
 { set +x; } 2>/dev/null # don't print the echo command, and don't print the 'set +x' itself
 
