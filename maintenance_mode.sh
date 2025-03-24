e71216d7e0f6e11c895bc73bce785cb49d64d1f2
Niklas Mohrin <dev@niklasmohrin.de>
2025-03-24 20:59:59 +0100

    Don't move maintenance files around

--- maintenance_mode.sh
+++ maintenance_mode.sh
@@ -7,14 +7,12 @@ MAINTENANCE_CONF=evap-maintenance.conf
 
 case $1 in
     enable)
-        cp --verbose "$(dirname $0)"/maintenance/* /var/www/html/
         a2ensite -q "$MAINTENANCE_CONF"
         a2dissite -q "$MAIN_CONF"
         service apache2 restart
         echo "Maintenance mode enabled."
         ;;
     disable)
-        rm --verbose --force /var/www/html/*
         a2ensite -q "$MAIN_CONF"
         a2dissite -q "$MAINTENANCE_CONF"
         service apache2 restart
